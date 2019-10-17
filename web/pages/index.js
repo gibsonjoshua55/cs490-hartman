2705b1979accedd3f962ee0b006c329f4d22cbbd
Zoe Larson <zlarson13@gmail.com>
2019-10-16 20:08:52 -0400

    Made a grid with data from sanity.

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import Layout from '../components/Layout'
-import { GreekCard } from '../components/greek-card';
+import { ShieldCard, AthenaCard, GreekCard } from '../components/greek-card';
 import client from '../client';
+import Grid from '@material-ui/core/Grid';
 
 const cardQuery = `
-  *[_type == "card"]
+  *[_type == "card"] {
+    "imageUrl": image.asset->url,
+    ...
+  }
 `
 
 class IndexPage extends React.Component {
@@ -24,13 +28,34 @@ class IndexPage extends React.Component {
 
   render () {
     const {cards} = this.props;
+
+    const card = cards[0];
+    console.log(card)
     return (
       <Layout
       config= {{
-        title: "A new Page"
+        title: "Page Title yo"
       }} >
         <h1> Here is some content</h1>
-        <GreekCard></GreekCard>
+        <Grid container spacing={3}>
+        {
+          cards.map( (card) => {
+            return (
+              <Grid item xs={12} md={6} lg={4} key={card.slug.current}>
+              <GreekCard 
+                
+                config={{
+                  image: card.imageUrl,
+                  title: card.title,
+                  cardtitle: card.title,
+                  descrip: card.description
+                }}
+              ></GreekCard>
+              </Grid>
+            )
+          })}
+          </Grid>
+        }
       </Layout>
     )
   }
